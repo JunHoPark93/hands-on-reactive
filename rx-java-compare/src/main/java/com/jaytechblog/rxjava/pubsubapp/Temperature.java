e9bbf9d0274f7738d3908a5a2dad900f9bc57e88
JunHoPark93 <pjh374968@gmail.com>
2020-12-26 16:35:21 +0900

    pub sub example with spring event listener

---
+++ rx-java-compare/src/main/java/com/jaytechblog/rxjava/pubsubapp/Temperature.java
@@ -0,0 +1,13 @@
+package com.jaytechblog.rxjava.pubsubapp;
+
+public class Temperature {
+    private final double value;
+
+    public Temperature(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
